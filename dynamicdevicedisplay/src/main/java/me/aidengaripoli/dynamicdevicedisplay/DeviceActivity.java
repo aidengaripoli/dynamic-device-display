d49949ab9b5fb33392f4d8faaca9375d83058faa
Daniel Milner <danielmmilner@gmail.com>
2019-04-15 14:56:30 +1000

    Created a UiGenerator class

--- dynamicdevicedisplay/src/main/java/me/aidengaripoli/dynamicdevicedisplay/DeviceActivity.java
+++ dynamicdevicedisplay/src/main/java/me/aidengaripoli/dynamicdevicedisplay/DeviceActivity.java
@@ -2,26 +2,14 @@ package me.aidengaripoli.dynamicdevicedisplay;
 
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.util.Log;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.Toast;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-
+import java.io.IOException;
 import java.io.InputStream;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 
 import me.aidengaripoli.dynamicdevicedisplay.elements.ButtonGroupFragment;
 import me.aidengaripoli.dynamicdevicedisplay.elements.DirectionalArrowsFragment;
@@ -48,107 +36,24 @@ public class DeviceActivity extends FragmentActivity implements
 
     private static final String TAG = "DeviceActivity";
 
-    private FragmentManager fragmentManager;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        int rootLayoutId = View.generateViewId();
+        FragmentManager fragmentManager = getSupportFragmentManager();
         ScrollView scrollView = new ScrollView(this);
-        LinearLayout rootLayout = new LinearLayout(this);
+        UiGenerator uiGenerator = new UiGenerator(fragmentManager);
 
-        scrollView.addView(rootLayout);
-
-        rootLayout.setOrientation(LinearLayout.VERTICAL);
-        rootLayout.setId(rootLayoutId);
-
-        fragmentManager = getSupportFragmentManager();
-
-        // XML parsing
-        // TODO: refactor xml parsing of UI to another class
         try {
             InputStream inputStream = getAssets().open("ui.xml");
-
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            Document document = builder.parse(inputStream);
-
-            Element deviceElement = document.getDocumentElement();
-            deviceElement.normalize();
-
-            // get all of the <group> elements
-            NodeList groupNodeList = document.getElementsByTagName("group");
-
-            // iterate through all the <group> elements
-            for (int i = 0; i < groupNodeList.getLength(); i++) {
-                Node node = groupNodeList.item(i);
-
-                if (node.getNodeType() == Node.ELEMENT_NODE) {
-                    Element groupElement = (Element) node;
-
-                    // generate a layout for each group
-                    ViewGroup groupLayout = generateGroupLayout(groupElement);
-
-                    // add the layout to the root layout
-                    rootLayout.addView(groupLayout);
-                }
-            }
-        } catch (Exception e) {
-            Log.e(TAG, "ERROR WHILST GENERATING UI: " + e.getMessage());
+            LinearLayout rootLayout = uiGenerator.generateUi(this, inputStream);
+            scrollView.addView(rootLayout);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.MATCH_PARENT
-        );
 
         // add the root layout to the content view
-        setContentView(scrollView, layoutParams);
-    }
-
-    private ViewGroup generateGroupLayout(Element element) {
-        NodeList guiNodeList = element.getElementsByTagName("gui_element");
-
-        // TODO: remove hardcoded linear layout
-        int layoutId = View.generateViewId();
-        LinearLayout layout = new LinearLayout(this);
-        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT
-        );
-        layoutParams.setMargins(40, 20, 0, 20);
-        layout.setOrientation(LinearLayout.HORIZONTAL);
-        layout.setLayoutParams(layoutParams);
-        layout.setId(layoutId);
-
-        // iterate through all the <gui_element> elements in the group
-        for (int i = 0; i < guiNodeList.getLength(); i++) {
-            Node node = guiNodeList.item(i);
-
-            if (node.getNodeType() == Node.ELEMENT_NODE) {
-                Element guiElement = (Element) node;
-
-                // generate a view (widget) for each gui_element
-                Fragment fragment = generateGuiElement(guiElement);
-
-                // add the view to the groups layout
-                if (fragment != null) {
-                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-
-                    fragmentTransaction.add(layoutId, fragment);
-                    fragmentTransaction.commit();
-                }
-            }
-        }
-        return layout;
-    }
-
-    private Fragment generateGuiElement(Element element) {
-        XmlDataExtractor xmlDataExtractor = new XmlDataExtractor();
-        String type = xmlDataExtractor.getElementType(element);
-
-        return ElementsFactory.getElement(type, element);
+        setContentView(scrollView);
     }
 
     @Override
@@ -177,7 +82,6 @@ public class DeviceActivity extends FragmentActivity implements
                 Toast.LENGTH_SHORT
         ).show();
     }
-
 
     @Override
     public void onFragmentInteraction(Uri uri) {
