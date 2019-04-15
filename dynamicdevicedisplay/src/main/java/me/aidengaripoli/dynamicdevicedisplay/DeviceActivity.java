77997829441760701b61566d79110806c74a4209
Daniel Milner <danielmmilner@gmail.com>
2019-04-15 14:26:12 +1000

    Created an ElementsFactory

--- dynamicdevicedisplay/src/main/java/me/aidengaripoli/dynamicdevicedisplay/DeviceActivity.java
+++ dynamicdevicedisplay/src/main/java/me/aidengaripoli/dynamicdevicedisplay/DeviceActivity.java
@@ -48,17 +48,6 @@ public class DeviceActivity extends FragmentActivity implements
 
     private static final String TAG = "DeviceActivity";
 
-    private static final String TOGGLE = "toggle";
-    private static final String PROGRESS = "progress";
-    private static final String SELECTION = "selection";
-    private static final String SLIDER = "slider";
-    private static final String PLUS_MINUS = "plusminus";
-    private static final String DIRECTIONAL_ARROWS = "directionalarrows";
-    private static final String SWITCH_TOGGLE = "switchtoggle";
-    private static final String STATUS = "status";
-    private static final String INPUT = "input";
-    private static final String BUTTON_GROUP = "buttongroup";
-
     private FragmentManager fragmentManager;
 
     @Override
@@ -159,53 +148,7 @@ public class DeviceActivity extends FragmentActivity implements
         XmlDataExtractor xmlDataExtractor = new XmlDataExtractor();
         String type = xmlDataExtractor.getElementType(element);
 
-        // return an element based on xml element type
-        switch (type) {
-            case TOGGLE: {
-                return ToggleFragment.newInstance(element);
-            }
-
-            case PROGRESS: {
-                return ProgressFragment.newInstance(element);
-            }
-
-            case SELECTION: {
-                return SelectionFragment.newInstance(element);
-            }
-
-            case SLIDER: {
-                return SliderFragment.newInstance(element);
-            }
-
-            case PLUS_MINUS: {
-                return PlusMinusFragment.newInstance(element);
-            }
-
-            case DIRECTIONAL_ARROWS: {
-                return DirectionalArrowsFragment.newInstance(element);
-            }
-
-            case SWITCH_TOGGLE: {
-                return SwitchToggleFragment.newInstance(element);
-            }
-
-            case STATUS: {
-                return StatusFragment.newInstance(element);
-            }
-
-            case INPUT: {
-                return InputFragment.newInstance(element);
-            }
-
-            case BUTTON_GROUP: {
-                return ButtonGroupFragment.newInstance(element);
-            }
-
-            default: {
-                // TODO: handle invalid element type
-                return null;
-            }
-        }
+        return ElementsFactory.getElement(type, element);
     }
 
     @Override
